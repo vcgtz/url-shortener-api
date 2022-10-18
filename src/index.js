5361e92ca89e01fbc1740c123172022f9c4bd831
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-10-18 14:24:45 -0500

    Add & configure ExpressJS

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const startApp = () => {
+  app.get('/', (req, res) => {
+    res.send('URL Shortener API');
+  });
+
+  app.listen(port, () => {
+    console.log(`Running on port: ${port}`);
+  })
+};
+
+startApp();
