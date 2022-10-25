6f169bd9b6c25722955c1dc32dc35b2ef9254e57
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-10-24 22:27:06 -0500

    Add sample tests to check the endpoint to generate a shortened url

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
-const connect = async () => {
+const connect = async (options = {}) => {
+  const { hideLogs } = options;
+
   try {
     await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log('Database Online');
+    if (!hideLogs) {
+      console.log('Database Online');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const disconnect = async () => {
+  try {
+    await mongoose.disconnect();
   } catch (err) {
     console.error(err);
   }
@@ -12,4 +24,5 @@ const connect = async () => {
 
 module.exports = {
   connect,
+  disconnect,
 };
---
+++ tests/api.test.js
@@ -0,0 +1,27 @@
+const { describe, it, expect, beforeEach, afterEach } = require('@jest/globals');
+const request = require('supertest');
+const mongoose = require('../src/config/mongoose');
+const ShortenedUrl = require('../src/models/shortenedUrl');
+const app = require('../src/server');
+
+describe('Testing the API to create shortened urls', () => {
+  beforeEach(async () => {
+    await mongoose.connect({ hideLogs: true });
+  });
+
+  it('generates a shortened url', async () => {
+    const response = await request(app)
+      .post('/shortener')
+      .send({ url: 'https://vicentegtz.com/about/' });
+
+    expect(response.status).toBe(200);
+    expect(response.body.status).toBe('ok');
+  });
+
+  afterEach(async () => {
+    await ShortenedUrl.deleteMany({
+      source: 'https://vicentegtz.com/about/',
+    }).exec();
+    await mongoose.disconnect();
+  });
+});
